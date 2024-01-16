ca1829a754365b87876cabb8fcaf045bd9e6f62a
Sintayehu Mulugeta <sintayehumulugeta77@gmail.com>
2024-01-16 14:11:25 +0300

    A beatifull code that passes the betty check

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - Simple shell entry point
+ * @ac: Count the command line argument to the shell
+ * @argv: The command line argument of the shell
+ *
+ * Return: 0 on success -1 otherwise
+ */
+int main(int ac, char *argv[])
+{
+	/* Main body statement */
+	return (0);
+}
